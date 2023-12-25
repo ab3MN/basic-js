5a1c6670d79e3ebcb7ab20dff4a35ffd20a600a7
Mykhailo Nikolaiev <ab3mn3@gmail.com>
2023-12-25 11:45:09 +0200

    feat:add solution with some bug:) to repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -2,24 +2,42 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  /* ==================== OPTIONS ==================== */
+  const repeatTimes = options?.repeatTimes ?? 1;
+  const additionRepeatTimes = options?.additionRepeatTimes ?? 1;
+  const addition = options?.addition ?? '';
+  const separator = options?.separator ?? '+';
+  const additionSeparator = options?.additionSeparator ?? '|';
+
+  /* ==================== ADDITIONS ==================== */
+  const additions = [];
+  for (let i = 0; i < additionRepeatTimes; i++) {
+    additions.push(addition);
+  }
+  const additionsString = additions.join(additionSeparator);
+
+  /* ==================== RES ==================== */
+  const res = [];
+  for (let j = 0; j < repeatTimes; j++) {
+    res.push(str + additionsString);
+  }
+  return res.join(separator);
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -3,33 +3,71 @@ const { NotImplementedError } = require('../extensions/index.js');
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.alphabet = '';
+    this.tabulaRecta = [];
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  generateTabulaRecta(alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ') {
+    this.alphabet = alphabet.split('');
+    for (let i = 0; i < alphabet.length; i++) {
+      let row = alphabet.slice(i);
+      row += alphabet.slice(0, i);
+      this.tabulaRecta.push(row);
+    }
+  }
+
+  repeatString(firstString, secondString) {
+    if (firstString.length > secondString.length) {
+      secondString += secondString;
+      return this.repeatString(firstString, secondString);
+    }
+    return secondString.slice(0, firstString.length);
+  }
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
+    let result = '';
+    let newKey = this.repeatString(message, key);
+    this.generateTabulaRecta();
+
+    for (let it = 0; it < message.length; it++) {
+      let i = this.alphabet.indexOf(message[it].toUpperCase());
+      let j;
+      if (i === -1) {
+        result += ' ';
+      } else {
+        j = this.alphabet.indexOf(newKey[it].toUpperCase());
+        result += this.tabulaRecta[i][j];
+      }
+    }
+    return result;
+  }
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!');
+    }
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
